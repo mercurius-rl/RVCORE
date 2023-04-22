8e8f83ff83aa6820928dc042448fe7ba066e7470
mercurius-rl <mercie.tech@gmail.com>
2023-04-22 18:47:34 +0900

    Update simulation sources

--- gen.bat
+++ gen.bat
@@ -1,4 +1,4 @@
-IF %1==n iverilog tb_core.v ncore.v alu.v aludec.v branch.v comp.v csr.v datapath.v decoder.v dmem.v icache.v pc.v regfile.v
-IF %1==p iverilog tb_core.v pcore.v alu.v aludec.v branch.v comp.v csr.v datapath.v decoder.v dmem.v icache.v pc.v regfile.v
+IF %1==n iverilog tb_core.v ncore.v alu.v aludec.v branch.v comp.v csr.v datapath.v decoder.v dmem.v icache.v pc.v regfile.v malu.v vector/valu.v vector/vma.v vector/vector_ex.v 
+IF %1==p iverilog tb_core.v pcore.v alu.v aludec.v branch.v comp.v csr.v datapath.v decoder.v dmem.v icache.v pc.v regfile.v malu.v vector/valu.v vector/vma.v vector/vector_ex.v 
 vvp a.out
 gtkwave d.vcd
